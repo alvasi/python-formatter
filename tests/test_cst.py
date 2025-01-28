c14908baec3309d2b24182287eb349bea3dc3d33
Alva Si <alva.si19@imperial.ac.uk>
2025-01-28 13:56:58 +0800

    test: setup unit test file

---
+++ tests/test_cst.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestSyntaxTree(unittest.TestCase):
+
+    def cst_parses_python_code_to_syntax_tree(self):
+        pass
+
+    def cst_compiles_syntax_tree_to_python_code(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
