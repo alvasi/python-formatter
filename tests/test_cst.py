8a8164eae2fbda428f2a5a3ceaf2aaf6dce85745
Alva <117771880+alvasi@users.noreply.github.com>
2025-01-29 17:10:35 +0800

    docs: rename tests

--- tests/test_cst.py
+++ tests/test_cst.py
@@ -1,12 +1,12 @@
 import unittest
 
 
-class TestSyntaxTree(unittest.TestCase):
+class TestParser(unittest.TestCase):
 
-    def cst_parses_python_code_to_syntax_tree(self):
+    def parses_python_code_to_syntax_tree(self):
         pass
 
-    def cst_compiles_syntax_tree_to_python_code(self):
+    def compiles_syntax_tree_to_python_code(self):
         pass
 
 
